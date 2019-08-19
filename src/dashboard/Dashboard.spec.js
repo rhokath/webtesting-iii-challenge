e1b3696500391830d8ea869fd07632a4f064ea58
Kathryn Rhoads <kathrynrhoads@Kathryns-MacBook-Pro-2.local>
2019-08-19 19:03:01 -0400

    mvp

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,35 @@
 // Test away
+import React from 'react';
+import {render, fireEvent} from 'react-testing-library';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard/>', ()=> {
+    it('renders without crasing', ()=> {
+        const {getByText} = render(<Dashboard/>)
+    })
+    it('cannot be closed or opened if it locked is true', ()=> {
+        const {queryByText} = render(<Dashboard/>);
+        queryByText(/locked/i);
+        expect(queryByText(/opened/i)).toBeFalsy();
+        expect(queryByText(/closed/i)).toBeFalsy();
+    })
+    it('buttons text change', ()=> {
+        const click = jest.fn();
+        const {getByText} = render(<Dashboard/>);
+        const open = getByText(/close gate/i);
+        fireEvent.click(open);
+        getByText(/open gate/i);
+        const lock = getByText(/lock gate/i)
+        fireEvent.click(lock);
+        getByText(/unlock gate/i);
+        const unlock = getByText(/unlock gate/i);
+        fireEvent.click(unlock);
+        getByText(/lock gate/i);
+        getByText(/open gate/i);
+        const openGate = getByText(/open gate/i)
+        fireEvent.click(openGate);
+        getByText(/close gate/i);
+
+
+    })
+})
