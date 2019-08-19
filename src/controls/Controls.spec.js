5aadc10b2259d8a78145ba925e6728ab689932fe
Kathryn Rhoads <46656679+rhokath@users.noreply.github.com>
2019-08-19 19:03:01 -0400

    mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Controls from "./Controls";
+describe("<Controls />", () => {
+   it("testing button text", () => {
+     const closeSpy = jest.fn();
+     const { queryByText, getByText } = render(<Controls locked={false} closed={false} toggleClosed={closeSpy} toggleLocked={closeSpy}/>);
+     const lockButton = queryByText(/lock gate/i);
+     expect(lockButton.disabled).toBe(true);
+     fireEvent.click(lockButton);
+     const closeButton = queryByText(/close gate/i);
+     expect(closeButton.disabled).toBe(false);
+     fireEvent.click(closeButton);
+     expect(closeSpy).toBeCalled();
+   });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'react';
+import {render} from 'react-testing-library';
+import Display from './Display.js';
+describe('<Display/>', () => {
+    it ('Display initally open and unlocked', () => {
+        const { queryByText } = render(<Display/>);
+        expect(queryByText(/unlocked/i)).toBeTruthy;
+        expect(queryByText(/open/i)).toBeTruthy;
+    });
+    it('get those props', () => {
+        const { getByText } = render(<Display locked={true} closed={true}/>);
+        expect(getByText(/locked/i));
+        expect(getByText(/closed/i));
+    });
+ });
